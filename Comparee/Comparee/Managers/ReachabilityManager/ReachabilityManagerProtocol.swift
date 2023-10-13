1547b2c88d576ccca58ef36f11f2657aa0017b35
Sinbanbonn <logvinov0607@icloud.com>
2023-10-13 16:00:43 +0300

    Code refactoring

--- Comparee/Comparee/Managers/ReachabilityManager/ReachabilityManagerProtocol.swift
+++ Comparee/Comparee/Managers/ReachabilityManager/ReachabilityManagerProtocol.swift
@@ -7,17 +7,17 @@
 
 import Foundation
 
-/// Protocol for managing user-related operations in Firebase.
+/// Protocol for checking user internet connection.
 protocol ReachabilityManagerProtocol {
     var isReachable: Bool { get }
 }
 
 private struct ReachabilityManagerKey: InjectionKey {
-    // The initial value for the injection key is an instance of AuthManager.
+    // The initial value for the injection key is an instance of ReachabilityManager.
     static var currentValue: ReachabilityManagerProtocol = ReachabilityManager()
 }
 
-/// Extension to manage the injection of AuthManager.
+/// Extension to manage the injection of ReachabilityManager.
 extension InjectedValues {
     var reachabilityManager: ReachabilityManagerProtocol {
         get { Self[ReachabilityManagerKey.self] }
